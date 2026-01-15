1f66e7c2aa54fc248030c207f5289f95d9f6840d
Росе Ѓорѓески <gjorgjeskir@gmail.com>
2026-01-15 09:21:07 +0100

    1

---
+++ src/smetkakg/smetkakg.java
@@ -0,0 +1,17 @@
+package smetkakg;
+
+public class smetkakg {
+
+	public static void main(String[] args) {
+		int Jabolkikg = 2 * 32;
+		int Kompirikg = 5 * 20;
+		double Praskikg = 2.5 * 50;
+		System.out.println("Smetka:");
+        System.out.println("Jabolki:  " + Jabolkikg + " denari");
+        System.out.println("Kompiri: " + Kompirikg + " denari");
+        System.out.println("Praski:  " + Praskikg + " denari");
+
+
+	}
+
+}
